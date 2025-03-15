4812ab7c40efb2667408cbcc1469fe79a0c9602a
Tivrus <tivruspod4@gmail.com>
2025-03-16 02:55:28 +0300

    new commit

--- app.py
+++ app.py
@@ -1,57 +1,37 @@
-from imports._import_ import *
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-TELEGRAM_TOKEN = 'YOUR_BOT_TOKEN'
-CHAT_ID = 'YOUR_CHAT_ID'
+server = Flask(__name__)
 
-class Application(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    comment = db.Column(db.Text, nullable=True)
+server.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db.db"
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+db=SQLAlchemy(server)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.json
 
-    # Создаем запись в БД
-    new_application = Application(
-        name=data.get('name'),
-        surname=data.get('surname'),
-        phone=data.get('phone'),
-        comment=data.get('comment', '')
-    )
-    db.session.add(new_application)
-    db.session.commit()
+@server.route("/")
+def main():
+    return render_template("index.html", float=" ", type=" ", rarity=" ",
+    name="USP-S | Cortex", wear="Прямо с завода", number_to_unlock="5",
+    days_to_unlock="дней", price="100 993.98", picture="../static/USP.png", 
+    title="USP-S | Cortex")
 
-    # Формируем сообщение для Telegram
-    message = (
-        f"НОВАЯ ЗАЯВКА!\n"
-        f"ID: {new_application.id}\n"
-        f"Имя: {data.get('name', 'Не указано')}\n"
-        f"Фамилия: {data.get('surname', 'Не указана')}\n"
-        f"Телефон: {data.get('phone', 'Не указан')}\n"
-        f"Комментарий: {data.get('comment', 'Нет')}"
-    )
-    
-    # Отправляем в Telegram
-    requests.get(
-        f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage',
-        params={'chat_id': CHAT_ID, 'text': message}
-    )
-    
-    return jsonify(success=True)
+@server.route("/marketCart")
+def marketCart():
+    return render_template("services.html", title="Cart", Ava="<img src="" alt="">")
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
+server.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
